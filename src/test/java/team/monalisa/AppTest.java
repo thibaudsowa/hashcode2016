41b4a77c6c46cc740e3490f90eb9bddf865f706a
Guillaume BILLEY <guillaume.billey@pharmagest.com>
2015-04-22 09:31:54 +0200

    [TEST](Testing input conversion)

--- src/test/java/team/monalisa/AppTest.java
+++ src/test/java/team/monalisa/AppTest.java
@@ -1,7 +1,14 @@
 package team.monalisa;
 
+import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
+import org.junit.Ignore;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Unit test for simple App.
@@ -12,8 +19,28 @@ public class AppTest
      * Rigourous Test :-)
      */
     @Test
-    public void testApp()
+    @Ignore
+    public void testApp() throws IOException
     {
-        assertTrue( true );
+        List<String> lines = new ArrayList<>();
+        lines.add("2 5 1 2 5");
+        lines.add("0 0");
+        lines.add("3 10");
+        lines.add("3 10");
+        lines.add("2 5");
+        lines.add("1 5");
+        lines.add("1 1");
+        DataCenter dataCenter = new DataCenter(lines);
+
+        assert(dataCenter.getR().equals(2));
+        assert(dataCenter.getS().equals(5));
+        assert(dataCenter.getU().equals(1));
+        assert(dataCenter.getP().equals(2));
+        assert(dataCenter.getM().equals(5));
+
+//        assertThat(dataCenter.getRaws().get(0));
+        Assertions.assertThat(dataCenter.getRaws().get(0)).containsExactly(false,true,true,true,true);
+        Assertions.assertThat(dataCenter.getRaws().get(1)).containsExactly(true, true, true, true, true);
+
     }
 }
