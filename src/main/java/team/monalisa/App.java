17700f4727e03928333e0e9f48ba8130fe2d888a
Thibaud Sowa <thibaudsowa@gmail.com>
2016-02-11 14:23:11 +0100

    [FEAT](chore): remet à zéro le projet

--- src/main/java/team/monalisa/App.java
+++ src/main/java/team/monalisa/App.java
@@ -3,25 +3,17 @@ package team.monalisa;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        Stream<String> inputLines = null;
         try {
             List<String> lines = Files.readAllLines(Paths.get(args[0]));
-            DataCenter dataCenter = new DataCenter(lines);
         } catch (IOException e) {
-            System.out.println("Le fichier "+args[0]+" ne semble pas exister.");
+            System.out.println("Le fichier " + args[0] + " ne semble pas exister.");
             System.out.println("Usage : java -jar hashcode.jar file");
         }
     }
--- src/test/java/team/monalisa/AppTest.java
+++ src/test/java/team/monalisa/AppTest.java
@@ -1,46 +1,21 @@
 package team.monalisa;
 
-import org.assertj.core.api.Assertions;
-import org.assertj.core.groups.Tuple;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigourous Test :-)
      */
     @Test
-    @Ignore
-    public void testApp() throws IOException
-    {
-        List<String> lines = new ArrayList<>();
-        lines.add("2 5 1 2 5");
-        lines.add("0 0");
-        lines.add("3 10");
-        lines.add("3 10");
-        lines.add("2 5");
-        lines.add("1 5");
-        lines.add("1 1");
-        DataCenter dataCenter = new DataCenter(lines);
-
-        assert(dataCenter.getR().equals(2));
-        assert(dataCenter.getS().equals(5));
-        assert(dataCenter.getU().equals(1));
-        assert(dataCenter.getP().equals(2));
-        assert(dataCenter.getM().equals(5));
-
-//        assertThat(dataCenter.getRaws().get(0));
-        Assertions.assertThat(dataCenter.getRaws().get(0)).containsExactly(false,true,true,true,true);
-        Assertions.assertThat(dataCenter.getRaws().get(1)).containsExactly(true, true, true, true, true);
-
+    public void testApp() throws IOException {
+        assertThat(true).isTrue();
     }
 }
